b3e0515bae23a7193099254c9ff3355636ae08ae
balamurali55 <balamuralismart5@gmail.com>
2024-06-21 16:47:38 +0530

    redeploye

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,29 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    
-    
+      colors: {
+        // Custom colors
+        primary: "#1D4ED8",
+        secondary: "#9333EA",
+        accent: "#FBBF24",
+        muted: "#6B7280",
+      },
+      fontFamily: {
+        // Custom fonts
+        sans: ['"Inter"', 'sans-serif'],
+        serif: ['"Merriweather"', 'serif'],
+      },
+      animation: {
+        // Custom animations
+        spin: "spin 2s linear infinite",
+        fadeInOut: "fadeInOut 2s ease-in-out infinite",
+      },
+      keyframes: {
+        fadeInOut: {
+          "0%, 100%": { opacity: 0, transform: "translateY(-20px)" },
+          "50%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
     },
   },
   plugins: [],
